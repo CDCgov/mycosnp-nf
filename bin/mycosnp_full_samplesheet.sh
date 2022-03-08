868811f16b19176e41e3e40f7de5f905d4104da1
Cipriano <rrn8@cdc.gov>
2022-03-08 14:08:40 -0500

    wrapper script for multiple dirs for sample sheet creator

---
+++ bin/mycosnp_full_samplesheet.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+script=bin/mycosnp_create_sample_sheet.pl
+echo "sample,r1,r2,r3,r4"
+for DIR in `ls $1`; do
+    if [[ -d "$1/$DIR" ]]; then
+        $script -i $1/$DIR -f
+    fi
+done | sort 
