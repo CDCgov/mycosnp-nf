87b1d3bce60af4987020a8f7356346b75cd8d83e
Cipriano <rrn8@cdc.gov>
2022-03-08 17:47:20 -0500

    make so run directory does not matter

--- bin/mycosnp_full_samplesheet.sh
+++ bin/mycosnp_full_samplesheet.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-script=bin/mycosnp_create_sample_sheet.pl
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+script=$SCRIPT_DIR/mycosnp_create_sample_sheet.pl
 echo "sample,r1,r2,r3,r4"
 $script -i $1 -f
 for DIR in `ls $1`; do
