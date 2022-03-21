d586cbf9886297d8fbad865e28a58850c3032fd9
CJ Jossart <cjjossart@gmail.com>
2022-03-21 14:00:54 -0500

    Cleaned up and annotated qc_report_stats.py

--- bin/qc_report_stats.py
+++ bin/qc_report_stats.py
@@ -4,7 +4,7 @@ from importlib.resources import path
 import argparse
 import pandas as pd
 
-# Arguement parser
+# Arguement parser: get arguments from FAQCS text files
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument("--sample")
 parser.add_argument("--stats", type=argparse.FileType("r"))
@@ -18,7 +18,7 @@ args = parser.parse_args()
 # Sample name variable
 sample_name = args.sample
 
-# Reference length variable
+# Reference length variable calculated from reference file
 header = None
 length = 0
 for line in args.reference:
@@ -33,7 +33,7 @@ for line in args.reference:
     else:
         length += len(line)
 
-# Stats report variables
+# Parse through stats.txt file for qc report variables
 list = []
 for lines in args.stats:
     list.append(lines)
@@ -46,7 +46,7 @@ unpaired_reads_after_trim = list[11].split(": ")[1].strip("\n")
 coverage_numer = float(read_length_before_trim) * float(reads_before_trim)
 coverage = coverage_numer / length
 
-# base_content_before_trim
+# Calculate the GC content from base_conent.txt file using Pandas
 df1 = pd.read_csv(
     args.base_content_before_trim, delim_whitespace=True, header=None, index_col=None
 )
@@ -54,11 +54,12 @@ df1_subset = df1[df1[0] == "GC"]
 df1_subset[3] = df1[1] * df1[2]
 sum_reads = sum(df1_subset[2])
 sum_reads_GC_content = sum(df1_subset[3])
+# Formatting, 2 decimal places and adding percent sign
 GC_content_before = (sum_reads_GC_content) / (sum_reads)
 GC_content_before = "{:.2f}".format(GC_content_before)
 GC_content_before = str(GC_content_before + "%")
 
-# base_content_after_trim
+# Calculate the GC content from base_conent.txt file using Pandas
 df2 = pd.read_csv(
     args.base_content_after_trim, delim_whitespace=True, header=None, index_col=None
 )
@@ -66,25 +67,28 @@ df2_subset = df2[df2[0] == "GC"]
 df2_subset[3] = df2[1] * df2[2]
 sum_reads = sum(df2_subset[2])
 sum_reads_GC_content = sum(df2_subset[3])
+# Formatting, 2 decimal places and adding percent sign
 GC_content_after = (sum_reads_GC_content) / (sum_reads)
 GC_content_after = "{:.2f}".format(GC_content_after)
 GC_content_after = str(GC_content_after + "%")
 
-# qual_scores_before_trim
+# Calculate the average phred/quality score from qual_scores.txt file using Pandas
 df3 = pd.read_csv(args.qual_scores_before_trim, delim_whitespace=True, index_col=None)
 df3["x"] = df3["Score"] * df3["readsNum"]
 sum_reads_num = sum(df3["readsNum"])
 phred_avg_before = sum(df3["x"]) / sum_reads_num
+# Formatting. 2 decimal points
 phred_avg_before = "{:.2f}".format(phred_avg_before)
 
-# qual_scores_after_trim
+# Calculate the average phred/quality score from qual_scores.txt file using Pandas
 df4 = pd.read_csv(args.qual_scores_after_trim, delim_whitespace=True, index_col=None)
 df4["x"] = df4["Score"] * df4["readsNum"]
 sum_reads_num = sum(df4["readsNum"])
 phred_avg_after = sum(df4["x"]) / sum_reads_num
+# Formatting. 2 decimal points
 phred_avg_after = "{:.2f}".format(phred_avg_after)
 
-# Preparing output list
+# Preparing output list with variables and then reformatting into a string
 output_string = ""
 output_list = [
     sample_name,
@@ -99,7 +103,7 @@ output_list = [
     str(coverage),
 ]
 
-# Creating tab delimited string for qc report generating
+# Creating tab delimited string for qc report generation
 for item in output_list:
     output_string += str(item) + "\t"
 print(output_string)
