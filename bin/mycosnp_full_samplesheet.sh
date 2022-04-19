50736cdcbfbac376ad18c70f805118a09e69a343
Cipriano <rrn8@cdc.gov>
2022-04-19 15:04:34 -0400

    changed samplesheet to accept multiple directories as arguments

--- bin/mycosnp_full_samplesheet.sh
+++ bin/mycosnp_full_samplesheet.sh
@@ -3,9 +3,12 @@
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 script=$SCRIPT_DIR/mycosnp_create_sample_sheet.pl
 echo "sample,r1,r2,r3,r4"
-$script -i $1 -f
-for DIR in `ls $1`; do
-    if [[ -d "$1/$DIR" ]]; then
-        $script -i $1/$DIR -f
-    fi
-done | sort 
+for VAR in "$@"
+do
+    $script -i $VAR -f
+    for DIR in `ls $VAR`; do
+        if [[ -d "$VAR/$DIR" ]]; then
+            $script -i $VAR/$DIR -f
+        fi
+    done
+done | sort
