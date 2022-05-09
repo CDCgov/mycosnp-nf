735e0f3b29c87db96fee19913be5136f4c08d419
Cipriano <rrn8@cdc.gov>
2022-05-09 12:01:14 -0400

    update for recursive search of directories

--- bin/mycosnp_full_samplesheet.sh
+++ bin/mycosnp_full_samplesheet.sh
@@ -3,12 +3,11 @@
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 script=$SCRIPT_DIR/mycosnp_create_sample_sheet.pl
 echo "sample,r1,r2,r3,r4"
+
 for VAR in "$@"
 do
     $script -i $VAR -f
-    for DIR in `ls $VAR`; do
-        if [[ -d "$VAR/$DIR" ]]; then
-            $script -i $VAR/$DIR -f
-        fi
+    for DIR in `find $VAR -type d`; do
+        $script -i $DIR -f
     done
-done | sort
+done | sort | uniq
