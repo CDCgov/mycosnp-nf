982b09dd97fb443dcedb90055cc5d237aab6fb82
Zack Mudge <zmudge@gatech.edu>
2025-03-24 13:40:30 -0400

    Edits to calculations and fields in pre-mycosnp-stats.sh

--- bin/pre-mycosnp-stats.sh
+++ bin/pre-mycosnp-stats.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# pre-mycosnp-stats.sh v1.0.0
-# Author: Jared Johnson, jared.johnson@doh.wa.gov
+# pre-mycosnp-stats.sh v1.1.0
+# Authors:
+# Jared Johnson, jared.johnson@doh.wa.gov
+# Zack Mudge, ZMudge@cdc.gov
 
-version="v1.0.0"
+version="v1.1.0"
 
 # inputs
 sample=$1
@@ -16,7 +18,7 @@ faqcs_qual=$5
 # help message
 if [ ${sample} == "-h" ] || [ ${sample} == "--help" ] || [ ${sample} == "-help" ]
 then
-    echo -e "subtyper.sh [sample_name] [path/to/sample_assembly] [path/to/ref_assembly] [path/to/faqcs/*.stats.txt] [path/to/faqcs/*.for_qual_histogram.txt]" && exit 0
+    echo -e "pre-mycosnp-stats.sh [sample_name] [path/to/sample_assembly] [path/to/ref_assembly] [path/to/faqcs/*.stats.txt] [path/to/faqcs/*.for_qual_histogram.txt]" && exit 0
 fi
 
 # version
@@ -33,22 +35,20 @@ gzip -d ${assembly} ${ref} || true
 sample_length=$(cat ${assembly%.gz} | grep -v ">" | tr -d '\n\t\r ' | wc -c)
 # reference assembly length
 ref_length=$(cat ${ref%.gz} | grep -v ">" | tr -d '\n\t\r ' | wc -c)
-# sample % GC
+# sample assembly % GC, to 2 decimal places
 sample_gc_count=$(cat ${assembly%.gz} | grep -v ">" | grep -Eo "G|C" | wc -l)
-sample_gc_perc=$(echo -e "${sample_gc_count}\t${sample_length}" | awk '{print 100*$1/$2}')
-# reference %GC
+sample_gc_perc=$(echo -e "${sample_gc_count}\t${sample_length}" | awk '{printf "%.2f", 100*$1/$2}')
+# reference %GC, to 2 decimal places
 ref_gc_count=$(cat ${ref%.gz} | grep -v ">" | grep -Eo "G|C" | wc -l)
-ref_gc_perc=$(echo -e "${ref_gc_count}\t${ref_length}" | awk '{print 100*$1/$2}')
+ref_gc_perc=$(echo -e "${ref_gc_count}\t${ref_length}" | awk '{printf "%.2f", 100*$1/$2}')
 
 #----- READ STATS -----#
 # total trimmed reads
 trmd_reads=$(cat ${faqcs_stats} | grep 'Reads #:' | sed -n 2p | cut -f 3 -d ' ')
-# total trimmed bases
-trmd_bases=$(cat ${faqcs_stats} | grep 'Total bases:' | sed -n 2p | cut -f 3 -d ' ')
-# average trimmed read Phred score
-avg_phred=$(cat ${faqcs_qual} | awk '{print $3,$1*$3}' | awk '{bases += $1} {qual += $2} END {print qual/bases}')
-# estimated average depth of coverage
-est_depth=$((trmd_bases/ref_length))
+# average trimmed read Phred score, to 2 decimal places
+avg_phred=$(cat ${faqcs_qual} | awk '{print $2,$1*$2}' | awk '{reads += $1} {qual += $2} END {printf "%.2f", qual/reads}')
+# estimated average depth of coverage, to 1 decimal place
+est_depth=$(echo "$trmd_reads $ref_length" | awk '{printf "%.1f", $1 / $2}')
 
 #----- OUTPUT -----#
-echo "${sample},${trmd_reads},${avg_phred},${est_depth},${sample_length},${ref_length},${sample_gc_perc},${ref_gc_perc}"
+echo "${trmd_reads},${avg_phred},${sample_length},${sample_gc_perc},${ref_length},${est_depth},${ref_gc_perc}"
